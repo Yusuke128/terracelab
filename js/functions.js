df7c888433e763ca09d1a9769650914190b50d31
Yusuke128 <bass.kyusuke@gmail.com>
2026-01-07 03:46:57 +0900

    reformat-stylelint,product,howto,adminvoice

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,38 @@
-const pickup_slider = new Swiper(".pickup-swiper", {
+const pickup_slider = new Swiper('.pickup-swiper', {
   loop: true,
   centeredSlides: true,
   spaceBetween: 28,
-  slidesPerView: "auto",
+  slidesPerView: 'auto',
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 4000,
+    speed: 600,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
   }
-  // autoplay: {
-  //   delay: 4000,
-  //   speed: 600,
-  //   disableOnInteraction: false,
-  //   pauseOnMouseEnter: true
-  // }
 });
 
 //ハンバーガーメニュー
-const hum = jQuery(".humberger");
-const header = jQuery(".header");
-const body = jQuery("body");
+const hum = jQuery('.humberger');
+const header = jQuery('.header');
+const body = jQuery('body');
 
-hum.on("click", function () {
-  const isOpen = header.hasClass("open");
+hum.on('click', function () {
+  const isOpen = header.hasClass('open');
 
-  header.toggleClass("open", !isOpen);
-  header.attr("data-state", isOpen ? "closed" : "open");
-  hum.attr("aria-expanded", String(!isOpen));
-  body.toggleClass("no-scroll", !isOpen);
+  header.toggleClass('open', !isOpen);
+  header.attr('data-state', isOpen ? 'closed' : 'open');
+  hum.attr('aria-expanded', String(!isOpen));
+  body.toggleClass('no-scroll', !isOpen);
 });
 
 // SP版でナビリンククリック時にメニューを閉じる
-jQuery(".link").on("click", function () {
-  if (header.hasClass("open")) {
-    header.removeClass("open");
-    body.removeClass("no-scroll");
+jQuery('.link').on('click', function () {
+  if (header.hasClass('open')) {
+    header.removeClass('open');
+    body.removeClass('no-scroll');
   }
 });
